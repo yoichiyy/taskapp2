fcd0b686c3b48a1dff1b9c8fed0c11e140f2ac0f
your git username <your git email>
2021-01-17 16:13:01 +0900

    「今日のタスク」バーを作成

--- lib/pages/task_list.dart
+++ lib/pages/task_list.dart
@@ -4,8 +4,20 @@ class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("タスクリスト"),
+      body: Container(
+        constraints: BoxConstraints.expand(height: 50),
+        child: Center(
+          child: Text(
+            "今日のタスク",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.grey[850],
+          borderRadius: BorderRadius.circular(5),
+        ),
       ),
     );
   }
