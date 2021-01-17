0fe0b27f67b7895e49a17f376bb8acc45528f8c8
your git username <your git email>
2021-01-17 16:14:42 +0900

    SafeAreaで囲んで、ステータスバーと被らないようにする

--- lib/pages/task_list.dart
+++ lib/pages/task_list.dart
@@ -4,19 +4,21 @@ class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        constraints: BoxConstraints.expand(height: 50),
-        child: Center(
-          child: Text(
-            "今日のタスク",
-            style: TextStyle(
-              color: Colors.white,
+      body: SafeArea(
+        child: Container(
+          constraints: BoxConstraints.expand(height: 50),
+          child: Center(
+            child: Text(
+              "今日のタスク",
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
           ),
-        ),
-        decoration: BoxDecoration(
-          color: Colors.grey[850],
-          borderRadius: BorderRadius.circular(5),
+          decoration: BoxDecoration(
+            color: Colors.grey[850],
+            borderRadius: BorderRadius.circular(5),
+          ),
         ),
       ),
     );
