088a3b5a49c83334707d60282b22f36c709f645e
5ffcc423d364960139fef429 <yoichiyy@gmail.com>
2021-01-19 19:41:09 +0900

    1/19 bar()を作った。関数利用して、共通化した

--- lib/pages/task_list.dart
+++ lib/pages/task_list.dart
@@ -5,19 +5,28 @@ class TaskList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Center(
-              child: Text(
-            "今日のタスク",
-            style: TextStyle(color: Colors.white),
-          )),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: Colors.grey,
-          ),
-          constraints: BoxConstraints.expand(height: 50),
+        child: Column(
+          children: [
+            bar("今日のタスク"),
+            bar("明日以降のタスク"),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget bar(String title) {
+    return Container(
+      child: Center(
+          child: Text(
+        title, //共通化して、関数を利用した
+        style: TextStyle(color: Colors.white),
+      )),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.grey,
+      ),
+      constraints: BoxConstraints.expand(height: 50),
     );
   }
 }
