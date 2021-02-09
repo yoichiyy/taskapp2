6996afdcd6da266419a0b488a44c4ba96867815a
5ffcc423d364960139fef429 <yoichiyy@gmail.com>
2021-02-09 20:04:36 +0900

    名前を保存する関数を作った

--- lib/model/model_task.dart
+++ lib/model/model_task.dart
@@ -1,105 +1,17 @@
-//import 'package:flutter/material.dart';
-//import 'package:task_app/Util/util_dialog.dart';
-//import 'package:task_app/Util/util_shareprefs.dart';
-//
-//class ModelTask extends ChangeNotifier {
-//  String name;
-//  ModelTask({this.name});
-//
-//  var db = FirebaseFirestore.instance;
-//  bool isLoading = false;
-//
-//  startLoading() {
-//    isLoading = true;
-//    notifyListeners();
-//  }
-//
-//  endLoading() {
-//    isLoading = false;
-//    notifyListeners();
-//  }
-//
-//  void saveTask(BuildContext context, String documentID) {
-//    if (documentID == null) {
-//      // 新規追加
-//      db.collection("Task").add({
-//        "name": this.name,
-//        "deadline": this.deadline,
-//        "repeat": this.repeat,
-//        "tag": this.tag,
-//        "dairyDate": this.dairyDate,
-//        "complete": this.complete,
-//        "createdAt": DateTime.now(),
-//        "updatedAt": DateTime.now(),
-//        "deviceID": UtilSharePrefs.getDeviceId()
-//      }).then((value) {
-//        print("==== タスク保存　成功 ====");
-//        if (this.repeat["durationDays"] != null) {
-//          DateTime _deadline = this.deadline;
-//          DateTime _deadlineAfterYear = this.deadline.add(Duration(days: 30));
-//
-//          while (_deadline.millisecondsSinceEpoch <
-//              _deadlineAfterYear.millisecondsSinceEpoch) {
-//            _deadline =
-//                _deadline.add(Duration(days: this.repeat["durationDays"]));
-//
-//            if (_deadline.millisecondsSinceEpoch <
-//                _deadlineAfterYear.millisecondsSinceEpoch) {
-//              db.collection("Task").add({
-//                "originalID": value.id,
-//                "name": this.name,
-//                "deadline": _deadline,
-//                "repeat": this.repeat,
-//                "tag": this.tag,
-//                "dairyDate": this.dairyDate,
-//                "complete": this.complete,
-//                "createdAt": DateTime.now(),
-//                "updatedAt": DateTime.now(),
-//                "deviceID": UtilSharePrefs.getDeviceId()
-//              }).then((value) {
-//                print("==== リピートタスク保存　成功 ====");
-//              });
-//            }
-//          }
-//        }
-//        Navigator.pop(context);
-//      }).catchError((error) {
-//        print("==== タスク保存　失敗 ====");
-//        print("==== エラー=> $error ====");
-//      });
-//    } else {
-//      db.collection("Task").doc(documentID).update({
-//        "name": this.name,
-//        "deadline": this.deadline,
-//        "repeat": this.repeat,
-//        "tag": this.tag,
-//        "dairyDate": this.dairyDate,
-//        "complete": this.complete,
-//        "updatedAt": DateTime.now(),
-//        "deviceID": UtilSharePrefs.getDeviceId()
-//      }).then((value) {
-//        print("==== タスク保存　成功 ====");
-//        Navigator.pop(context);
-//        if (this.complete) {
-//          UtilDialog().okDialog(context, "完了", "「${this.name}」を完了しました。");
-//        }
-//      }).catchError((error) {
-//        print("==== タスク保存　失敗 ====");
-//        print("==== エラー=> $error ====");
-//      });
-//    }
-//  }
-//
-//  Stream indexTasks() {
-//    return db
-//        .collection("Task")
-//        .where("deviceID", isEqualTo: UtilSharePrefs.getDeviceId())
-//        .where("complete", isEqualTo: false)
-//        .orderBy("deadline")
-//        .snapshots();
-//  }
-//
-//  void deleteData(String documentId) {
-//    db.collection("Task").doc(documentId).delete();
-//  }
-//}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ModelTask extends ChangeNotifier {
+  var db = FirebaseFirestore.instance; //データ保存場所と接続しました
+
+  void saveTask({String name}) {
+    //void→関数実行後にdata残さない ⇔  stringとか
+    db.collection("task").add({
+      "name": name,
+    }).then((value) {
+      print("タスク保存成功！");
+    }).catchError((error) {
+      print("タスク保存失敗");
+    });
+  }
+}
