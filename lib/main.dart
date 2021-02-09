57996f707f27b567595a94b76e6b886a8c2228ec
5ffcc423d364960139fef429 <yoichiyy@gmail.com>
2021-02-09 19:43:04 +0900

    firebase 多分成功

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'home.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
